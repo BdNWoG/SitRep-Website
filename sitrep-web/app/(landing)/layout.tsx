e00e27c467c777366a6e616bc708fa86ce3b830c
BdNWoG <billy.gao@hotmail.com>
2024-05-29 17:42:02 -0700

    landing

--- sitrep-web/app/(landing)/layout.tsx
+++ sitrep-web/app/(landing)/layout.tsx
@@ -2,7 +2,7 @@ const LandingLayout = ({
     children 
 }: { children: React.ReactNode }) => {
     return (
-        <main className="h-full bg-[#111827] overflow-auto">
+        <main className="h-full bg-[#334f8e] overflow-auto">
             <div className="mx-auto max-w-screen-xl h-full w-full">
                 {children}
             </div>
--- sitrep-web/components/landing/landing-hero.tsx
+++ sitrep-web/components/landing/landing-hero.tsx
@@ -11,22 +11,25 @@ export const LandingHero = () => {
         <div className="text-white font-bold py-36 text-center space-y-5">
             <div className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl space-y-5 font-extrabold">
                 <h1>The Best AI Tool for </h1>
-                <div className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
+                <div className="text-transparent bg-clip-text bg-gradient-to-r from-yellow-400 to-red-600">
                     Personalized Financial Information
                 </div>
             </div>
-            <div className="text-sm md:text-xl font-light text-zinc-400">
-                Create Content Using AI 10x Better!
+            <div className="text-sm md:text-xl font-light text-stone-400">
+                The Sit Rep you Deserve while you have your Morning Coffee
+            </div>
+            <div className="text-sm md:text-xl font-light text-stone-400">
+                You are on a Mission, and We are Reporting for Duty
             </div>
             <div>
                 <Link href={isSignedIn? "/dashboard" : "/sign-up"}>
                     <Button variant="premium" className="md:text-lg p-4 md:p-6 rounded-full font-semibold">
-                        Start Generating For Free
+                        Get Your Personalized Financial Video
                     </Button>
                 </Link>
             </div>
             <div className="text-zinc-400 text-xs md :text-sm font-normal">
-                No Credit Card Required!
+                Free Tier and Waitlist Available Now
             </div>
         </div>
     )
